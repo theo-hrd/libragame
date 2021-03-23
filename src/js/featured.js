0b8e95e0e5f79d47d4094c7a9fff4ad821d1380f
Théo <theo.hrd02@gmail.com>
2021-03-23 16:26:04 +0100

    first page

---
+++ src/js/featured.js
@@ -0,0 +1,18 @@
+let render = document.getElementById('games');
+const url = "https://api.rawg.io/api/games?key=10afd979e0874030811ad36e60da2bda";
+
+async function genre(){
+    const req = await fetch(url);
+    const data = await req.json();
+    let games = data.results;
+    // games.forEach(game => console.log(game.background_image))
+    console.log(games);
+    games.slice(0,3).forEach(game => {
+        document.getElementById('featured_game_card').insertAdjacentHTML('beforeend', `
+            <h3> ${game.name} </h3>   
+            <img src="${game.background_image}">
+        `)
+    })
+        // console.log(data.results[0].name); bon retour de la request avec just le nom
+}
+genre();
