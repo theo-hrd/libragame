cafd8466daf4c112ad4608a8e3717cf0e9995517
theo-hrd <theo.hrd02@gmail.com>
2021-03-27 10:13:09 +0100

    .env done

--- app/Models/ContactManager.php
+++ app/Models/ContactManager.php
@@ -6,7 +6,7 @@ class ContactManager extends Database{
 
     public function sendMessageToDb($username,$email,$message){
         $database = $this->dbConnect();
-        $req = $database->prepare("INSERT INTO `contact` ('username', 'email', 'msg') VALUES (?, ?, ?);");
+        $req = $database->prepare("INSERT INTO `contact` (`username`, `email`, `msg`) VALUES (?, ?, ?);");
         $req->execute(array($username,$email,$message));
         return $req;
     }
--- app/Models/Database.php
+++ app/Models/Database.php
@@ -1,18 +1,22 @@
 <?php
-
 namespace Project\Models;
-
-require('vendor/autoload.php');
-
 use Exception;
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 
 class Database{
 
-
     protected function dbConnect(){
         
+        $db_host = $_ENV['DB_HOST'];
+        $db_name = $_ENV['DB_NAME'];
+        $db_user = $_ENV['DB_USER'];
+        $db_pass = $_ENV['DB_PASS'];
+
         try{
-            $database = new \PDO("mysql:host=localhost;dbname=test;charset=utf8", 'root', 'admin');
+            $database = new \PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
             return $database;
         } 
         catch(Exception $e){
