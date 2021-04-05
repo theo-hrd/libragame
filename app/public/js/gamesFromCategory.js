59ec10d500cc8b051a6d8ea1210128f883a6e210
theo-hrd <theo.hrd02@gmail.com>
2021-04-05 15:56:02 +0200

    loop for gamesFromCategory.js

--- app/public/js/gamesFromCategory.js
+++ app/public/js/gamesFromCategory.js
@@ -4,8 +4,7 @@ async function gamesFromCategory(){
     document.getElementById('button').addEventListener('click', loadGameFromCategory);
 
     function loadGameFromCategory(){
-        console.log('button clicked !');
-
+        // instanciating the ajax object
         var xhr = new XMLHttpRequest();
         //  OPEN - type, url/file, async(true false)
         xhr.open('GET', 'https://api.rawg.io/api/games?genres=4&key=10afd979e0874030811ad36e60da2bda',true);
@@ -16,11 +15,14 @@ async function gamesFromCategory(){
                 console.log(category);
                 var output = '';
 
-                output +=
-                '<div class="game">'+
-                '<h2>' +category.results[0].name+ '</h2>'+
-                '<img src="'+category.results[0].background_image+'" class="img_game">'+
-                '</div>'
+                
+                for(let i=0; i< category.results.length; i++){
+                    output +=
+                    '<div class="game">'+
+                    '<h2>' +category.results[i].name+ '</h2>'+
+                    '<img src="'+category.results[i].background_image+'" class="img_game">'+
+                    '</div>'
+                }
             }
             document.getElementById('games').innerHTML = output;
         }
