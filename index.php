933a2ce635618f500c00b088cf3789c2aebb0573
theo-hrd <theo.hrd02@gmail.com>
2021-04-15 15:48:54 +0200

    refacto router from if else to switch case

--- index.php
+++ index.php
@@ -8,110 +8,161 @@ use Project\Controllers\Controller;
 use Project\Controllers\ViewController;
 
 try{
-    
+
     $controller = new Controller();
     $viewController = new ViewController();
-    
+
+
     if(isset($_GET['action'])){
+
+        switch($_GET['action']){
+    
+            // --------------------------------------------------------------------------
+            //  Views with no logic applied from the Controller
+            // --------------------------------------------------------------------------
+
+            // returning the homepage.php view
+            case 'homepage':
+                $viewController->homePage();
+                break;
+
+            // returning the userprofile.php view
+            case 'profile':
+                $userId = $_SESSION['id'];
+
+                $controller->profilePage($userId);
+                break;
+
+            // returning the allgames.php view
+            case 'allgames':
+                $viewController->allGamesPage();
+                break;
+
+
+            // --------------------------------------------------------------------------
+            //  Views and logic applied from the Controller for the games
+            // --------------------------------------------------------------------------
+
+
+            // returning the game.php view
+            case 'game':
+                $controller->SingleGamePage();
+                break;
+
+            // like games
+            case 'like':
+                $gameId = $_GET['id'];
+
+                $controller->likeGame($gameId);
+                break;
+
+
+
+
+            // --------------------------------------------------------------------------
+            // Views and logic applied from the Controller for the Contact page
+            // --------------------------------------------------------------------------
+
+            // returning the contact.php view
+            case 'contact':
+                $controller->contactPage();
+                break;
+
+            // CONTACT VIEW AND SENDER
+            case 'contactSender':
+                $username = htmlspecialchars($_POST['username']);
+                $email = htmlspecialchars($_POST['email']);
+                $message = htmlspecialchars($_POST['msg']);
+                    
+                $controller->contactSender($username, $email, $message);
+                break;
+
+
+
+            // --------------------------------------------------------------------------
+            // Views and logic applied from the Controller for the user
+            //  REGISTER/CONNECT/DISCONNECT/UPDATE USERNAME/DELETE USER (by order)
+            // --------------------------------------------------------------------------
+
+
+            // REGISTER
+            // returning the view to connect the user (register.php)
+            case 'register':
+                $controller->registerPage();
+                break;
+
+            // creating a new user in the database
+            case 'registerNewUser':
+                $username = htmlspecialchars($_POST['username']);
+                $email = htmlspecialchars($_POST['email']);
+                $confirmEmail = htmlspecialchars($_POST['confirmEmail']);
+                $password = htmlspecialchars($_POST['password']); 
+                $confirmPassword = htmlspecialchars($_POST['confirmPassword']);
         
-        if($_GET['action'] == 'homepage'){ // returning the homepage.php view
-            $viewController->homePage();
-        }
-
-        else if($_GET['action'] == 'profile'){ // returning the userprofile.php view
-            $userId = $_SESSION['id'];
-            $controller->profilePage($userId);
-        }
-
-        else if($_GET['action'] == 'allgames'){ // returning the allgames.php view
-            $viewController->allGamesPage();
-        }
+                $controller->registerNewUser($username, $email, $confirmEmail,$password, $confirmPassword);
+                break;
         
-        else if($_GET['action'] == 'game'){ 
-            $controller->SingleGamePage();
-        }
 
-        // CONTACT VIEW AND SENDER
-        else if($_GET['action'] == 'contact'){ // returning the contact.php view
-            $controller->contactPage();
-        }
-        else if($_GET['action'] == 'contactSender'){ //sending contact message 
-            
-            $username = htmlspecialchars($_POST['username']);
-            $email = htmlspecialchars($_POST['email']);
-            $message = htmlspecialchars($_POST['msg']);
-            
-            $controller->contactSender($username, $email, $message);
-        }
+            //  --- CONNECT ---
 
+            // returning the login.php view
+            case 'login':
+                $controller->loginPage();
+                break;
+        
+            // starting the session to connect the user
+            case 'connectUser':
+                $username = htmlspecialchars($_POST['username']);
+                $password = htmlspecialchars($_POST['password']);
+        
+                $controller->connectUser($username,$password);
+                break;
 
 
-        // USER REGISTER VIEW AND REGISTERING NEW USER
-        else if($_GET['action'] == 'register'){ // returning the register.php view
-            $controller->registerPage();
-        }
-
-        else if($_GET['action'] == 'registerNewUser'){ // creating a new user in the database
-            
-            $username = htmlspecialchars($_POST['username']);
-            $email = htmlspecialchars($_POST['email']);
-            $confirmEmail = htmlspecialchars($_POST['confirmEmail']);
-            $password = htmlspecialchars($_POST['password']); 
-            $confirmPassword = htmlspecialchars($_POST['confirmPassword']);
-
-            $controller->registerNewUser($username, $email, $confirmEmail,$password, $confirmPassword);
-        }
+            // --- DISCONNECT ---
+             // disconnecting the user 
+            case 'logout':
+                $controller->userLogout();
+                break;
 
 
-        // USER CONNECT AND DISCONNECT 
-        else if($_GET['action'] == 'login'){ // returning the login.php view
-            $controller->loginPage();
-        }
+            // --- UPDATE USERNAME ---
+            // updating profile name page
+            case 'updateProfileName':
+                $controller->updateProfileNamePage();
+                break;
 
-        else if($_GET['action'] == 'connectUser'){ // starting the session to connect the user
-            $username = htmlspecialchars($_POST['username']);
-            $password = htmlspecialchars($_POST['password']);
-
-            $controller->connectUser($username,$password);
-            
-        }
-
-        else if($_GET['action'] == 'logout'){ // returning the logout.php view
-            $controller->userLogout();
-        }
+            // form to update the name
+            case 'uploadProfileName':
+                $id = $_SESSION['id'];
+                $username = htmlspecialchars($_POST['username']);
+                $username = str_replace(' ', '', $username);
+        
+                $controller->updateProfileName($id, $username);
+                break;
 
 
-        // updating profile name page
-        else if($_GET['action'] == 'updateProfileName'){
-            $controller->updateProfileNamePage();
-        }
-        // form to update the name
-        else if($_GET['action'] == 'uploadProfileName'){
-            $id = $_SESSION['id'];
-            $username = htmlspecialchars($_POST['username']);
-            $username = str_replace(' ', '', $username);
-            $controller->updateProfileName($id, $username);
-        }
-        // deleting the profile
-        else if($_GET['action'] == 'deleteProfilePage'){
-            $controller->deleteUserProfilePage();
-        }
-        else if($_GET['action'] == 'confirmUserDelete'){
-            $id = $_SESSION['id'];
-            $selectChoice = $_POST['choice'];
-            $controller->deleteUser($id, $selectChoice);
-        }
-        // like games
-        else if($_GET['action'] == 'like'){
-            $gameId = $_GET['id'];
-            $controller->likeGame($gameId);
-        }
+            // --- DELETE USER ---
+            // returning the deleteprofile.php view
+            case 'deleteProfilePage':
+                $controller->deleteUserProfilePage();
+                break;  
+
+            // deleting the profile
+            case 'confirmUserDelete':
+                $id = $_SESSION['id'];
+                $selectChoice = $_POST['choice'];
+
+                $controller->deleteUser($id, $selectChoice);
+                break;
+        } 
 
     } else{
         $viewController->homePage();
     }
 
-
 } catch(Exception $e){
     die('Error: ' . $e->getMessage());
 }
+
+
